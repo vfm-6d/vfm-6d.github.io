b5057e872c24abc9309293c875d509022a2dff82
Kai Chen <kaichen@KaideMacBook-Pro.local>
2024-10-29 21:12:10 +0800

    add wild6d-camera v2 result

--- static/js/index.js
+++ static/js/index.js
@@ -153,7 +153,7 @@ function switchTab(evt, selectedTab) {
   if (selectedTab === 'wild6d') {
     addOptionsToDropdown([
       { value: 'wild6d-laptop.png', display: "laptop" },
-      { value: 'wild6d-camera.png.png', display: "camera" },
+      { value: 'wild6d-camera.png', display: "camera" },
       { value: 'real/env_2_combined.mp4', display: "mug" },
       { value: 'real/env_3_combined.mp4', display: "bottle" },
       { value: 'real/env_4_combined.mp4', display: "bowl" },
